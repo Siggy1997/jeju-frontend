25c3b54d2bbf407fbd507c2a88e67a4e2c4ea683
unknown <159753martin@naver.com>
2025-11-22 02:23:39 +0900

    preload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MainMap from "./pages/MainMap";
 import Main from "./pages/Main";
 import MusicPlayer from "./components/MusciPlayer";
+import { getFoodList } from "./apis/foodAPI";
 
 function App() {
+  const id = sessionStorage.getItem("id");
+  const [foodData, setFoodData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const foodResponse = await getFoodList(id, "food");
+      setFoodData(foodResponse);
+
+      // 이미지 미리 로드
+      foodResponse.foodList.forEach(item => {
+        [1, 2, 3, 4, 5].forEach(num => {
+          const img = new Image();
+          img.src = `/images/food/${item.seq}/pic${num}.jpeg`;
+        });
+      });
+
+      setLoading(false);
+    })();
+  }, [id]);
+
+  if (loading) return <div>로딩중...</div>;
+
+
   return (
-    <> 
-    <MusicPlayer />
-        <Routes> 
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/main" element={<Main />}></Route>
-          <Route path="/map" element={<MainMap />}></Route>
-        </Routes>
+    <>
+      <MusicPlayer />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/main" element={<Main />}></Route>
+        <Route path="/map" element={<MainMap />}></Route>
+      </Routes>
     </>
   );
 }
