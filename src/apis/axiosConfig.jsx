b0d9ec548020e656bf532c8c2c314341212fd8ca
unknown <159753martin@naver.com>
2025-11-22 13:14:44 +0900

    dd

--- src/apis/axiosConfig.jsx
+++ src/apis/axiosConfig.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // 공통 axios 인스턴스
 const axiosConfig = axios.create({
-  baseURL: "http://192.168.45.223:8082", // 기본 API URL
+  // baseURL: "http://192.168.45.223:8082", // 기본 API URL
   // baseURL: "http://192.168.219.168:8082", // 기본 API URL
-  // baseURL: "https://backend.siggy.co.kr", // 기본 API URL
+  baseURL: "https://backend.siggy.co.kr", // 기본 API URL
   // baseURL: "http://192.168.0.32:8082", // 기본 API URL
   timeout: 5000, // 5초 타임아웃 
   headers: {
